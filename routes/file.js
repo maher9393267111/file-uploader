d8b6b1d52b66796570da46388e20c44fd83dc742
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-02-11 23:17:43 +0300

    add error msg

--- routes/file.js
+++ routes/file.js
@@ -133,37 +133,10 @@ fileRouter.post("/upload", upload.single("image"), async (req, res) => {
     res.status(200).json({ fileName: fileName });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Catch Error: Internal Server Error." });
+    res.status(500).json({ message: "Catch Error: Internal Server Error.",erroMessage:error?.message });
   }
 });
 
-// Route to retrieve information about files from RDS
-fileRouter.get("/files", async (req, res) => {
-  try {
-    // Query file information from RDS
-    const sql = "SELECT * FROM files";
-    db.query(sql, (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ message: "Internal Server Error." });
-        return;
-      }
-
-      // Extract relevant information for the response
-      const fileData = result.map((file) => ({
-        filename: file.filename,
-        fileUrl: file.file_url,
-        // lastModified: file.last_modified,
-        // size: file.size / 1024 + "KB", // Adjust this based on your RDS schema
-      }));
-
-      res.status(200).json(fileData);
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error." });
-  }
-});
 
 // Route to delete a file
 fileRouter.post("/delete", async (req, res) => {
@@ -189,7 +162,7 @@ fileRouter.post("/delete", async (req, res) => {
 
 
 
-    
+
 
     // res.status(200).json({ message: "File deleted successfully." });
   } catch (error) {
