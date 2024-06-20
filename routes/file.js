34d293f1e668a631b6476767e7cd1c08bc977b56
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-06-20 05:57:35 +0300

    add

--- routes/file.js
+++ routes/file.js
@@ -3,7 +3,7 @@ const fileUpload = require("express-fileupload");
 const AWS = require("aws-sdk");
 const { db } = require("../configs/db");
 const multer = require("multer");
-const path =require('path')
+const path = require("path");
 
 const crypto = require("crypto");
 // const multer = require('multer');
@@ -18,7 +18,7 @@ const {
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const validFileTypes = [
-  "*"
+  "*",
   // "image/jpeg",
   // "image/jpg",
   // "image/png",
@@ -42,7 +42,6 @@ const validFileTypes = [
   // "video/x-flv",
 ];
 
-
 const storage = multer.memoryStorage();
 
 const upload = multer({
@@ -61,9 +60,9 @@ const fileRouter = express.Router();
 // prepare S3 client
 const bucketName = "dash93";
 const region = "us-east-1";
-const accessKeyId = "DO00PHUUAT4VXQF27H6N"
+const accessKeyId = "DO00PHUUAT4VXQF27H6N";
 // "DO00M9XA6DJ9P9Y4UWFT";
-const secretAccessKey = "P1YyD/tvykl7hpLKBF/g3Ff1KN2yJOunrRlWSXGRa5s"
+const secretAccessKey = "P1YyD/tvykl7hpLKBF/g3Ff1KN2yJOunrRlWSXGRa5s";
 // "fcWJxA4nn0r5yNKUi1011UzQ66FPMO6Lt8UEuGWSypE";
 
 const endpoint = "https://nyc3.digitaloceanspaces.com";
@@ -78,127 +77,178 @@ const s3Client = new S3Client({
   },
 });
 
-fileRouter.post("/upload", upload.single("image"), async (req, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).json({ message: "No files were uploaded." });
-    }
+// fileRouter.post("/upload", upload.single("image"), async (req, res) => {
+//   try {
+//     if (!req.file) {
+//       return res.status(400).json({ message: "No files were uploaded." });
+//     }
 
-    
-    const fileName = crypto.randomBytes(32).toString("hex");
+//     const fileName = crypto.randomBytes(32).toString("hex");
 
-    const size = parseInt(req.query.size);
-    const hieghtsize = parseInt(req.query.hieghtsize);
+//     const size = parseInt(req.query.size);
+//     const hieghtsize = parseInt(req.query.hieghtsize);
 
-    console.log("HEEEEE🐦  ❤️🐦  ❤️🐦  ❤️🐦  ❤️EEE", req.file);
+//     console.log("HEEEEE🐦  ❤️🐦  ❤️🐦  ❤️🐦  ❤️EEE", req.file);
 
-    const fileBuffer = await sharp(req.file.buffer)
-      .resize({
-        height: hieghtsize ? hieghtsize : 700,
-        width: size ? size : 1000,
-        fit: "cover",
-      })
-      .toBuffer();
+//     const fileBuffer = await sharp(req.file.buffer)
+//       .resize({
+//         height: hieghtsize ? hieghtsize : 700,
+//         width: size ? size : 1000,
+//         fit: "cover",
+//       })
+//       .toBuffer();
 
-    await s3Client.send(
-      new PutObjectCommand({
-        Bucket: "dash93",
-        Key: fileName,
-        Body: fileBuffer,
-        ContentType: req.file.mimetype,
+//     await s3Client.send(
+//       new PutObjectCommand({
+//         Bucket: "dash93",
+//         Key: fileName,
+//         Body: fileBuffer,
+//         ContentType: req.file.mimetype,
 
-        ACL: "public-read",
-      })
-    );
+//         ACL: "public-read",
+//       })
+//     );
 
-    //  collection = req.query.collection
-    const oldfile = req.query.oldfile;
+//     const oldfile = req.query.oldfile;
 
-    // const session = await getServerAuthSession({ req, res });
-    //const oldURL = session.user.image;
-    //onst oldKey = oldURL.substring(oldURL.lastIndexOf("/") + 1);
+//     if (oldfile) {
+//       await s3Client.send(
+//         new DeleteObjectCommand({
+//           Bucket: "dash93",
+//           Key: oldfile,
+//         })
+//       );
+//     }
 
-    //const newURL = `${env.NEXT_PUBLIC_BUCKET_URL}/${key}`;
+//     console.log("File uploaded successfully.");
+//     res.status(200).json({ fileName: fileName });
+//   } catch (error) {
+//     console.error(error);
+//     res
+//       .status(500)
+//       .json({
+//         message: "Catch Error: Internal Server Error.",
+//         erroMessage: error?.message,
+//       });
+//   }
+// });
 
-    if (oldfile) {
-      await s3Client.send(
-        new DeleteObjectCommand({
-          Bucket: "dash93",
-          Key: oldfile,
-        })
-      );
-    }
+// // Route to delete a file
+// fileRouter.post("/delete", async (req, res) => {
+//   const { link } = req.body;
 
-    // if (!result) {
-    //   return res
-    //     .status(500)
-    //     .json({ message: "Internal Server Error. S3 Location is undefined." });
-    // }
+//   try {
+//     // Find the file in the database
 
-    console.log("File uploaded successfully.");
-    res.status(200).json({ fileName: fileName });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Catch Error: Internal Server Error.",erroMessage:error?.message });
-  }
-});
+//     if (!link) {
+//       return res.status(400).json({ message: "No image to delete" });
+//     }
 
+//     if (link) {
+//       const deleteParams = {
+//         Bucket: "dash93",
+//         Key: link,
+//       };
+//       await s3Client.send(new DeleteObjectCommand(deleteParams));
 
-
-// Route to delete a file
-fileRouter.post("/delete", async (req, res) => {
-  const { link } = req.body;
-
-  try {
-    // Find the file in the database
-
-    if (!link) {
-      return res.status(400).json({ message: "No image to delete" });
-    }
-
-    if (link) {
-      const deleteParams = {
-        Bucket: "dash93",
-        Key: link,
-      };
-      await s3Client.send(new DeleteObjectCommand(deleteParams));
-
-      res.json({ message: "Image Deleted Successfully" });
-    }
-
-
-
-
-
-
-    // res.status(200).json({ message: "File deleted successfully." });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error." });
-  }
-});
+//       res.json({ message: "Image Deleted Successfully" });
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Internal Server Error." });
+//   }
+// });
 
 
 
 
 // ------------------------SLIDER------------
 
-fileRouter.post('/uploads', upload.array('images', 10), async (req, res) => {
+
+
+fileRouter.post('/upload', upload.single('image'), async (req, res) => {
   try {
-    if (!req.files || req.files.length === 0) {
-      return res.status(400).json({ error: 'No files were uploaded' });
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file was uploaded' });
     }
 
-    console.log("FILES" , req.files)
+    const file = req.file;
+    console.log("FILE-->" , file)
+    const fileName = `${crypto.randomBytes(32).toString('hex')}${path.extname(file.originalname)}`;
+    const size = parseInt(req.query.size);
+    const hieghtsize = parseInt(req.query.hieghtsize);
+
+    const fileBuffer = await sharp(file.buffer)
+      .resize({
+        height: hieghtsize ? hieghtsize : 800,
+        width: size ? size : 1000,
+        fit: 'fill',
+      })
+      .toBuffer();
+
+    await s3Client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: fileName,
+        Body: fileBuffer,
+        ContentType: file.mimetype,
+        ACL: 'public-read',
+      })
+    );
+
+    console.log('File uploaded successfully:', fileName);
+    res.status(200).json({ file: fileName });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error', message: error.message });
+  }
+});
+
+
+
+fileRouter.delete('/delete', async (req, res) => {
+  try {
+    const { fileName } = req.query;
+
+    console.log("DELETE BODY-->" , req.query)
+
+    if (!fileName) {
+      return res.status(400).json({ message: 'No file name provided' });
+    }
+
+
+    const deleteParams = {
+      Bucket: bucketName,
+      Key: fileName,
+    };
+
+    await s3Client.send(new DeleteObjectCommand(deleteParams));
+
+    console.log('File deleted successfully:', fileName);
+    res.json({ message: 'File deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+
+
+
+fileRouter.post("/uploads", upload.array("images", 10), async (req, res) => {
+  try {
+    if (!req.files || req.files.length === 0) {
+      return res.status(400).json({ error: "No files were uploaded" });
+    }
+
+    console.log("FILES", req.files);
     const uploadedFiles = [];
-
-
-
-
 
     // Process and upload the new files
     for (const file of req.files) {
-      const fileName = `${crypto.randomBytes(32).toString('hex')}${path.extname(file.originalname)}`;
+      const fileName = `${crypto.randomBytes(32).toString("hex")}${path.extname(
+        file.originalname
+      )}`;
       const size = parseInt(req.query.size);
       const hieghtsize = parseInt(req.query.hieghtsize);
 
@@ -206,7 +256,7 @@ fileRouter.post('/uploads', upload.array('images', 10), async (req, res) => {
         .resize({
           height: hieghtsize ? hieghtsize : 800,
           width: size ? size : 1000,
-          fit: 'fill',
+          fit: "fill",
         })
         .toBuffer();
 
@@ -216,33 +266,29 @@ fileRouter.post('/uploads', upload.array('images', 10), async (req, res) => {
           Key: fileName,
           Body: fileBuffer,
           ContentType: file.mimetype,
-          ACL: 'public-read',
+          ACL: "public-read",
         })
       );
 
       uploadedFiles.push(fileName);
     }
 
-    console.log('Files uploaded and deleted successfully.' ,uploadedFiles);
+    console.log("Files uploaded and deleted successfully.", uploadedFiles);
     res.status(200).json({ files: uploadedFiles });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error', message: error.message });
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", message: error.message });
   }
 });
 
-
-
-
-
 //DELETE ARRAY OF IMAGES
-
 
 fileRouter.post("/delets", async (req, res) => {
   const { filesToDelete } = req.body;
 
-  console.log("FILES TO DELETE" , filesToDelete ,"BODYYYY-->" , req.body)
-
+  console.log("FILES TO DELETE", filesToDelete, "BODYYYY-->", req.body);
 
   try {
     if (!filesToDelete || filesToDelete.length === 0) {
@@ -264,8 +310,6 @@ fileRouter.post("/delets", async (req, res) => {
   }
 });
 
-
-
-
+// -----------------
 
 module.exports = { fileRouter };
