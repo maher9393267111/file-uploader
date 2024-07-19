e8d64b83c789ab87c4ce86bde9aa2fecb86640eb
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-07-19 23:13:13 +0300

    add

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(express.static(__dirname + "/public", {maxAge: 0 }));
 app.use(cors());
 
 app.get("/", (req, res) => {
